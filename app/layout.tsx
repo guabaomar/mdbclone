7dc38ca46c335027fdc273aedec3a55fc2a323ec
Omar <omarguaba54@gmail.com>
2025-05-13 14:31:10 -0400

    Fonts updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Providers from "@/components/themecomp";
@@ -7,16 +6,6 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "DbmClone",
